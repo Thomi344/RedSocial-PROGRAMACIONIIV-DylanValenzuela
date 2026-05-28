5349e2b44cf9194e3f25df5f5e61162ed5496009
Dylan Thomas Valenzuela <thomasdylan05@hotmail.com>
2026-05-28 20:51:41 -0300

    Suma del Dist para deploy

--- greenpoint-front/src/app/environments/environment.example.ts
+++ greenpoint-front/src/app/environments/environment.example.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     cloudinaryCloudName: '_CLOUD_NAME_ACA',
-    cloudinaryPreset: '_PRESET_ACA'
+    cloudinaryPreset: '_PRESET_ACA',
+    apiUrl: 'http://localhost:3000/api'
 };
