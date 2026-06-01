51c5d8df07eaff7d57233ba082f85f94c8edce47
Dylan Thomas Valenzuela <thomasdylan05@hotmail.com>
2026-06-01 14:51:05 -0300

    fix: importar UsuariosModule en AutenticacionModule para inyeccion de modelo

--- greenpoint-back/src/modulos/autenticacion/autenticacion.module.ts
+++ greenpoint-back/src/modulos/autenticacion/autenticacion.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AutenticacionController } from './autenticacion.controller';
 import { AutenticacionService } from './autenticacion.service';
+import { UsuariosModule } from '../usuarios/usuarios.module';
 
 @Module({
+  imports: [UsuariosModule],
   controllers: [AutenticacionController],
   providers: [AutenticacionService],
 })
