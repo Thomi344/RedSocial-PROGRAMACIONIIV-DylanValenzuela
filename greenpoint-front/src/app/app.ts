6d69c537b3fed6b19b10f4147d8d54abfb582ed4
Dylan Thomas Valenzuela <thomasdylan05@hotmail.com>
2026-06-22 21:58:35 -0300

    feat: agregar pantalla de carga global y validación inicial de token

--- greenpoint-front/src/app/app.ts
+++ greenpoint-front/src/app/app.ts
@@ -1,12 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal,inject,OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet,Router } from '@angular/router';
 import {Navbar} from './componentes/navbar/navbar';
+import { Auth } from './servicios/auth';
+import { firstValueFrom } from 'rxjs';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [CommonModule,RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('GreenPoint');
+
+  private authService = inject(Auth);
+  private router = inject(Router);
+
+  // --- Estado de la aplicación ---
+  cargandoInicial = signal<boolean>(true);
+
+  async ngOnInit() {
+    const token = this.authService.obtenerToken();
+
+    // --- Si no hay token, redirigimos al login ---
+    if(!token) {
+      this.cargandoInicial.set(false);
+      this.router.navigate(['/login']);
+      return;
+    }
+    // --- Si hay token, validamos con el backend ---
+    try {
+      await firstValueFrom(this.authService.validarToken());
+      // --- Si el backend responde que el token es válido, seguimos en la app ---
+      this.cargandoInicial.set(false);
+      this.router.navigate(['/publicaciones']);
+    }catch (error) {
+      console.warn('Token inválido o expirado. Redirigiendo al login...');
+      this.authService.cerrarSesion(); // Borra la sesión corrupta
+      this.cargandoInicial.set(false);
+      this.router.navigate(['/login']);
+    }
+  }
 }
